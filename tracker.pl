be064a56d0a345c3c2dc18991ede735254c7f926
punytan <punytan@gmail.com>
2010-11-22 21:57:04 +0900

    defer tweet for 5 sec

--- tracker.pl
+++ tracker.pl
@@ -31,10 +31,13 @@ sub on_entry {
             my $file = $2;
 
             my $string = "$package by $author - http://frepan.64p.org/~$id/$file/";
-            $twitty->post('statuses/update', {status => $string}, sub {
-                print Dumper [scalar localtime, $_[1] ? $_[1]->{text} : \@_];
-                $_[1] ? undef : $twitty->post('statuses/update', {status => '@punytan error: post ' . time}, sub { print Dumper \@_});
-            });
+            my $w; $w = AE::timer 5, 0, sub {
+                $twitty->post('statuses/update', {status => $string}, sub {
+                    print Dumper [scalar localtime, $_[1] ? $_[1]->{text} : \@_];
+                    $_[1] ? undef : $twitty->post('statuses/update', {
+                        status => '@punytan error: post ' . time}, sub { print Dumper [time, $_[2]]});
+                });
+            };
 
         } else {
             print Dumper [scalar localtime, 'error: parse url', $entry, [$package, $author, $url]];
