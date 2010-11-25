f3508f7515a425a704975d402e991f75ae796f51
punytan <punytan@gmail.com>
2010-11-25 20:00:45 +0900

    use timer correctly

--- tracker.pl
+++ tracker.pl
@@ -13,7 +13,14 @@ while (1) {
     print Dumper [scalar localtime, 'start connecting'];
     my $client = AnyEvent::FriendFeed::Realtime->new(
         request    => "/feed/cpan",
-        on_entry   => \&on_entry,
+        on_entry   => sub {
+            my @args = @_;
+            my $w; $w = AE::timer 5, 0, sub {
+                print Dumper ['on entry', \@args];
+                on_entry(@args);
+                undef $w;
+            }
+        },
         on_error   => sub { print Dumper [scalar localtime, \@_]; $done->send; },
     );
     print Dumper [scalar localtime, 'recv'];
@@ -30,14 +37,12 @@ sub on_entry {
             my $id   = lc $1;
             my $file = $2;
 
-            my $string = "$package by $author - http://frepan.64p.org/~$id/$file/";
-            my $w; $w = AE::timer 5, 0, sub {
-                $twitty->post('statuses/update', {status => $string}, sub {
-                    print Dumper [scalar localtime, $_[1] ? $_[1]->{text} : \@_];
-                    $_[1] ? undef : $twitty->post('statuses/update', {
-                        status => '@punytan error: post ' . time}, sub {print Dumper [time, $_[2]]});
-                });
-            };
+            my $string = "\@punytan $package by $author - http://frepan.64p.org/~$id/$file/";
+            $twitty->post('statuses/update', {status => $string}, sub {
+                print Dumper [scalar localtime, $_[1] ? $_[1]->{text} : \@_];
+                $_[1] ? undef : $twitty->post('statuses/update', {
+                    status => '@punytan error: post ' . time}, sub {print Dumper [time, $_[2]]});
+            });
 
         } else {
             print Dumper [scalar localtime, 'error: parse url', $entry, [$package, $author, $url]];
