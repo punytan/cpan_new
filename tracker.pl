f0945ad3cff35f2b3632fb10a2698022999bb24d
punytan <punytan@gmail.com>
2011-06-22 17:35:14 +0900

    refctor

--- tracker.pl
+++ tracker.pl
@@ -5,36 +5,36 @@ use AnyEvent;
 use AnyEvent::HTTP;
 use AnyEvent::Twitter;
 use AnyEvent::FriendFeed::Realtime;
-
 our @Q;
 
 local $| = 1;
 my $OAuth  = do 'oauth.pl' or die $!;
 my $twitty = AnyEvent::Twitter->new(%$OAuth);
 
-while (1) {
-    my $done = AE::cv;
+our ($CONN, $CLIENT);
+my $w; $w = AE::timer 1, 10, sub {
+    return if $CLIENT->{guard};
 
-    warn Dumper [&now, 'start connecting'];
-    my $client = AnyEvent::FriendFeed::Realtime->new(
+    warn Dumper [AE::now, 'start connecting'];
+    $CLIENT = AnyEvent::FriendFeed::Realtime->new(
         request  => "/feed/cpan",
-        on_entry => \&on_entry,
+        on_entry => sub {
+            on_entry(@_);
+        },
         on_error => sub {
-            warn Dumper [&now, \@_];
-            $done->send;
+            warn Dumper [AE::now, \@_];
         },
     );
+};
 
-    my $qwatcher; $qwatcher = AE::timer 5, 300, sub {
-        my $string = shift @Q;
-        _tweet($string) if $string;
-    };
+my $qwatcher; $qwatcher = AE::timer 5, 300, sub {
+    my $string = shift @Q;
+    tweet($string) if $string;
+};
 
-    warn Dumper [&now, 'recv'];
-    $done->recv;
-}
+warn Dumper [AE::now, 'recv'];
 
-sub now { scalar localtime; }
+AE::cv->recv;
 
 sub on_entry {
     my $entry = shift;
@@ -49,48 +49,53 @@ sub on_entry {
                 $file = $1;
             }
 
-            my $frepan_url = sprintf 'http://metacpan.org/release/%s/%s/', uc($id), $file;
-
-            my $string = sprintf "%s by %s - %s", $package, $pauseid, $frepan_url;
+            my $metacpan = sprintf 'http://metacpan.org/release/%s/%s/', uc($id), $file;
+            my $string   = sprintf "%s by %s - %s", $package, $pauseid, $metacpan;
 
             if (length $string > 140) {
                 my %params = (
                     login   => 'cpannew',
                     apiKey  => 'R_b593e932246cfbe5625ec2ebb16647fc',
                     format  => 'json',
-                    longUrl => $frepan_url,
+                    longUrl => $metacpan,
                 );
 
                 AnyEvent::HTTP::http_get "http://api.bitly.com/v3/shorten", %params, sub {
                     my $json   = JSON::decode_json(shift);
                     my $string = sprintf "%s by %s - %s", $package, $pauseid, $json->{data}{url};
-                    _tweet($string);
+                    tweet($string);
                 };
 
             } else {
-                _tweet($string);
+                tweet($string);
             }
 
         } else {
-            $twitty->post('statuses/update', {status => '@punytan error: parse url ' . time}, sub {
-                warn Dumper [&now, 'error: parse url', $entry, [$package, $author, $url], [ time, $_[2] ]];
+            $twitty->post('statuses/update', {
+                status => '@punytan error: parse url ' . time
+            }, sub {
+                warn Dumper [AE::now, 'error: parse url', $entry, [$package, $author, $url], [ time, $_[2] ]];
             });
         }
 
     } else {
-        $twitty->post('statuses/update', {status => '@punytan error: parse body ' . time}, sub {
-            warn Dumper [&now, 'error: parse body', $entry, [ time, $_[2] ]];
+        $twitty->post('statuses/update', {
+            status => '@punytan error: parse body ' . time
+        }, sub {
+            warn Dumper [AE::now, 'error: parse body', $entry, [ time, $_[2] ]];
         });
     }
 };
 
-sub _tweet {
+sub tweet {
     my $string = shift;
-    $twitty->post('statuses/update', {status => $string}, sub {
+    $twitty->post('statuses/update', {
+        status => $string
+    }, sub {
         if ($_[1]) {
-            print Dumper [&now, "Send: $string", "Receive: $_[1]->{text}"];
+            print Dumper [AE::now, "Send: $string", "Receive: $_[1]->{text}"];
         } else {
-            warn Dumper [&now, "Send: $string", \@_];
+            warn Dumper [AE::now, "Send: $string", \@_];
             push @Q, $string; # on error, push it to queue
         }
     });
